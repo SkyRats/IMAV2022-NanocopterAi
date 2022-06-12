d9a533079ac923b6b37e436ba922d80881eb8b75
Igor Pontes Tresolavy <tresolavy@usp.br>
2022-06-12 16:04:32 -0300

    versão final do script

--- pulp-dronet/pulp-dronet-v2/modifyDataset_gray.bash
+++ pulp-dronet/pulp-dronet-v2/modifyDataset_gray.bash
@@ -8,21 +8,26 @@ echo "#    # #   #    #   #   #  #    #   #   #    # ";
 echo " ####  #    #   #   #    # #    #   #    ####  ";
 echo "------------------------------------------------"
 
-echo "Para que esse scrip funcione, é preciso executá-lo em um path que contenha uma pasta 'dataset'."
 echo "Primeiro checaremos se o computador possui o imagemagick. Se não, ele será instalado."
 sleep 2
 echo "----------------------------------------------------------------------------------------------------------"
 
+if [ "$(which convert)" == "" ]; then
+    echo "Você ainda não possui o pacote imagemagick. Instale-o a seguir:"
+    sudo apt install imagemagick
+else
+    echo "Você já possui o pacote imagemagick"
+fi
 
-sudo apt install imagemagick
-echo "----------------------------------------------------------------------------------------------------------"
-echo "Criando pasta 'output' onde as imagens serão depositadas."
-mkdir dataset/training/output_gray
 echo "----------------------------------------------------------------------------------------------------------"
 echo "Começando o processo!"
 
-for picture in dataset/fine_tuning/HMB_1/images/*.png; do
-    convert $picture -gravity South -crop 320x320+0+0 -grayscale Rec709Luminance -format jpeg $picture
+for picture in `find dataset/ -regex ".*\.\(png\)"`; do
+    convert $picture -gravity South -crop 320x320+0+0 -grayscale Rec709Luminance -format jpeg "${picture/.png/.jpeg}"
+done
+
+for picture in `find dataset/ -regex ".*\.\(jpg\)"`; do
+    convert $picture -gravity South -crop 320x320+0+0 -grayscale Rec709Luminance -format jpeg "${picture/.jpg/.jpeg}"
 done
 
 echo "Pronto!"
