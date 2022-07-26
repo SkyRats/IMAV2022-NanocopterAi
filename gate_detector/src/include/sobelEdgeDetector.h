cefda7cef03b42dc9ad50c9355750e5ce31a3682
Igor Pontes Tresolavy <tresolavy@usp.br>
2022-07-26 17:48:22 -0300

    modify sobelEdgeDetector header file

--- gate_detector/src/include/sobelEdgeDetector.h
+++ gate_detector/src/include/sobelEdgeDetector.h
@@ -1,8 +1,11 @@
 #ifndef __SOBEL_EDGE_DETECTOR__
 #define __SOBEL_EDGE_DETECTOR__
 #include "imageIO.h"
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
 
-void sobelEdgeContrast(PGMImage *img);
+PGMImage* sobelEdgeContrast(PGMImage *img, const uint8_t sobelMask[9]);
 
 #endif
 
