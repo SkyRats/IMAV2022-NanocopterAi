3a08e527c2e12e80a8fb5365ece69be31fcd33eb
Igor Pontes Tresolavy <tresolavy@usp.br>
2022-08-08 19:34:13 -0300

    update test_template.c

--- DORY_network/src/test_template.c
+++ DORY_network/src/test_template.c
@@ -3,7 +3,7 @@
  * Alessio Burrello <alessio.burrello@unibo.it>
  *
  * Copyright (C) 2019-2020 University of Bologna
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,11 +14,51 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 #include "network.h"
-// on fabric controller
-int main () {
+#include "pmsis.h"
+#include "stdio.h"
+
+/* Variables used. */
+int32_t * to_send;
+
+void test_uart_dronet(void)
+{
+    printf("Entering main controller\n");
+
+    uint32_t errors = 0;
+    struct pi_device uart;
+    struct pi_uart_conf conf;
+
+    /* Init & open uart. */
+    pi_uart_conf_init(&conf);
+    conf.enable_tx = 1;
+    conf.enable_rx = 0;
+    conf.baudrate_bps = 115200;
+    pi_open_from_conf(&uart, &conf);
+    if (pi_uart_open(&uart))
+    {
+        printf("Uart open failed !\n");
+        pmsis_exit(-1);
+    }
   	network_setup();
-  	network_run_FabricController();
+
+    for (uint8_t i=0; i<150; i++)
+    {
+        to_send = network_run_FabricController();
+        pi_uart_write(&uart, to_send, 8);
+        pi_time_wait_us(500000);
+    }
+
+    pi_uart_close(&uart);
+
+    pmsis_exit(errors);
+}
+
+/* Program Entry. */
+int main(void)
+{
+    printf("\n\n\t *** PMSIS Uart dronet test ***\n\n");
+    return pmsis_kickoff((void *) test_uart_dronet);
 }
