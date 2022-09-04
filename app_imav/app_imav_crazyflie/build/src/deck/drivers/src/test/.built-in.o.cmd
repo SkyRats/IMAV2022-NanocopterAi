028079b48cd2919c15ce426ad2eeb069503d938f
fegentil02 <fegentil02@gmail.com>
2022-09-04 06:34:12 -0300

    Gate detection behaviour add

---
+++ app_imav/app_imav_crazyflie/build/src/deck/drivers/src/test/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_src/deck/drivers/src/test/built-in.o :=  arm-none-eabi-gcc --specs=nosys.specs --specs=nano.specs -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -nostdlib   -r -o src/deck/drivers/src/test/built-in.o src/deck/drivers/src/test/exptest.o src/deck/drivers/src/test/exptestRR.o src/deck/drivers/src/test/exptestBolt11.o
